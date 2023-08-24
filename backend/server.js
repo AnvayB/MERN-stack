571e5ac0390131c155677f8f666fc23f272e2302
Anvay Bhanap <anvay.bhanap@gmail.com>
2023-08-23 17:28:59 -0700

    mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const color = require('colors')
 const cors = require('cors');
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 app.use(express.json())
